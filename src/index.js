1295bfa029a430c39118adb5e93195c7e1bcbe05
nichol88 <nichol88@gmail.com>
2020-03-27 00:16:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //let form = document.getElementById('create-task-form')
+  let submit = document.getElementsByTagName('input')[1]
+  submit.addEventListener('click', function(e) {
+    e.preventDefault();
+  })
 });
